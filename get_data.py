3016df91ab45b0080dda06b1ff67ad46dd6dd2c3
Dan <Danilaanis@gmail.com>
2023-12-21 06:55:44 -0300

    first commit

---
+++ get_data.py
@@ -0,0 +1,9 @@
+﻿import dvc.api
+
+modelpkl = dvc.api.read(
+    'twitter.csv',
+    repo='https://drive.google.com/drive/folders/1g_uUEWm2VRG0aOM42GtZp7G-m8f492ur',
+    mode='rb'
+)
+
+print(modelpkl)
